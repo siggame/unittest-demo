5b116a96e18f16951cb0f745f8e5401ec5df53c5
Kevin Lai <zlnh4@umsystem.edu>
2022-01-01 11:30:18 -0600

    fix duplicate mutations

--- unittests/mutation-candidates.py
+++ unittests/mutation-candidates.py
@@ -5,7 +5,7 @@ import sys
 from pycparser import parse_file
 import pycparser.c_ast as c_ast
 import os
-
+import collections
 
 class NodeVisitor(c_ast.NodeVisitor):
     
@@ -13,7 +13,8 @@ class NodeVisitor(c_ast.NodeVisitor):
         super().__init__()
         self.file = file
         self.context = ["<global>"]
-    
+        self.lines = collections.defaultdict(list)
+  
     def visit_FuncDef(self, node):
         if node.coord.file == "<stdin>":
             self.cur = node.decl.name
@@ -23,11 +24,13 @@ class NodeVisitor(c_ast.NodeVisitor):
 
 
     def visit_BinaryOp(self, node):
+        # During preprocessing, the part of the file that included the headers
+        # is tagged with <stdin>.
         if node.coord.file == "<stdin>":
             # Since we had to include _undef.h to remove gcc's compiler extensions,
             # remove a line
             line = node.coord.line - 1
-            print(line, node.coord.column, self.context[-1], "BinaryOp", node.op, file=self.file)
+            self.lines[line].append([line, node.coord.column, self.context[-1], "BinaryOp", node.op])
             self.visit(node.left)
             self.visit(node.right)
 
@@ -53,14 +56,39 @@ def main():
     except:
         print("err")
     else:
-        print("ok")
+        
+        with open(file, "r") as fp:
+            source = fp.read()
+        
+        src_lines = source.split("\n")
 
         # Record to .mut file the locations of mutation candidates
+        
         with open(out, "w") as fp:
             print(file, file=fp)
             visitor = NodeVisitor(fp)
             visitor.visit(ast)
 
+            # Annoyingly, the "node.coord" attribute only captures the root node
+            # coordinate. So if you have more than one operation per line, you
+            # end up with multiple entries in the mutation registry. So we need
+            # to manually search each line for the specified symbol and update
+            for line in visitor.lines:
+                src_line = src_lines[line - 1]
+                visited = set()
+                for _, col, func, op, sym in visitor.lines[line]:
+                    j = col
+                    symlen = len(sym)
+                    while True:
+                        if (src_line[j:j+symlen]) == sym:
+                            coord = (line, j, func, op, sym)
+                            if coord not in visited:
+                                visited.add(coord)
+                                break
+                        j+=1
+                for line in sorted(visited, key=lambda x: x[1]):
+                    print(*line, file=fp)
+                
     finally:
         pass
 
--- unittests/mutator.py
+++ unittests/mutator.py
@@ -1,5 +1,14 @@
+"""
+USAGE: c-mutate <mut> <srcdir> [options]
+
+Options
+    --config=<config>
+"""
 import sys
 import random
+import docopt
+import os
+
 BINOP_MUTATION_TYPE = {
     "&&": "LCR",
     "||": "LCR",
@@ -17,41 +26,52 @@ BINOP_MUTATION_TRANSITION = {
     "ROR": { "==", "!=", "<", "<=", ">", ">=" }
 }
 
+def candidate_mutations(sym):
+    symboltype = BINOP_MUTATION_TYPE[sym]
+    return BINOP_MUTATION_TRANSITION[symboltype]
+    
+
+MAX_PER_OPERATOR = 2
+MAX_CHANGE_PER_MUTATION = 1
+
+def mutate_line(line, col:int, sym:str, used:set):
+    candidates = candidate_mutations(sym)
+    repl = random.choice([ s for s in candidates if s not in used ])
+    used.add(repl)
+    front = line[:col]
+    back = line[col:].replace(sym, repl, 1)
+    return front + back, repl
+
 def main():
-    src, mut = sys.argv[1], sys.argv[2]
+    args = docopt.docopt(__doc__)
+    srcdir = args["<srcdir>"]
+    mut = args["<mut>"]    
     with open(mut, "r") as fp:
+        filename = fp.readline().strip()
         db = [ line.strip().split() for line in fp ]
+
+    with open(filename, "r") as fp:
+        code = fp.read()
     
-
+    for i, (line, col, func, op, sym) in enumerate(db):
+        line = int(line) - 1 # line numbers start at 1
+        col = int(col)
     
-    while True:
-        try:
-            executed_line = input()
-        except OSError:
-            break
-        else:
-            match = [ line for line in db if line[0] == executed_line ]
+        source = code.split("\n")
+        used = {sym}
 
-            with open(src, "r") as fp:
-                code = fp.read()
-            
-            for i, matched_operator in enumerate(match):
-                lines = [ line for line in code.split("\n") ]
-                _, col, _, op, sym = matched_operator
-                row = int(executed_line)
-                col = int(col)
-                symboltype = BINOP_MUTATION_TYPE[sym]
-                candidates = BINOP_MUTATION_TRANSITION[symboltype]
-                repl = random.choice([ s for s in candidates if s != sym ])
-                front = lines[row - 1][:col + 1]
-                back = lines[row - 1][col + 1:].replace(sym, repl, 1)
-                lines[row - 1] = front + back
-                new_source = "\n".join(lines)
+        # Generate a mutation for each operator, up to MAX_PER_OPERATOR or until
+        # we've exhausted the set of candidates
+        for j in range(MAX_PER_OPERATOR):
+            if len(used) == len(candidate_mutations(sym)):
+                break
+            source[line], alt = mutate_line(source[line][:], col, sym, used)
+            mutfile = os.path.join(srcdir, f"{i}-{j}-{filename}")
+            with open(os.path.join(srcdir, f"{i}-{j}-{filename}"), "w") as fp:
+                print("\n".join(source), file=fp)
 
-                with open(f".mut{i}-{row}.{col}-{src}", "w") as fp: 
-                    print(new_source, file=fp)
+            print(line+1, col, func, op, sym, alt, mutfile)
 
-                print(row, col, op, sym, repl)
-
+             
 if __name__ == "__main__":
     main()
